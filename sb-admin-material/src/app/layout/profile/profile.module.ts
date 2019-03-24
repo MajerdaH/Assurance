a3d1f29722d4ffcb6c0fcccbc4f18596fb93ec97
Meriem MEJERDA <majerdahejer@gmail.com>
2019-03-24 20:02:08 +0100

    first commit

---
+++ sb-admin-material/src/app/layout/profile/profile.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule as FormModule, ReactiveFormsModule } from '@angular/forms';
+import {
+    MatAutocompleteModule, MatCardModule, MatCheckboxModule, MatDatepickerModule,
+    MatFormFieldModule, MatInputModule, MatNativeDateModule, MatRadioModule, MatSelectModule,
+    MatSliderModule, MatSlideToggleModule
+} from '@angular/material';
+@NgModule({
+  declarations: [ProfileComponent],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    CommonModule,
+    MatAutocompleteModule,
+    FormModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSliderModule,
+    FlexLayoutModule.withConfig({addFlexToParent: false})
+  ]
+})
+
+export class ProfileModule { }
